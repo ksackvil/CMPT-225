c363eaaa3f83b5bf188390ed1dbde75e45f0dbff
Kai Sackville-Hii <sackvillehiik@gmail.com>
2019-01-10 13:57:20 -0800

    first commit

---
+++ labs/lab0/hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (void) {
+    int inputNum = 0;
+
+    cin >> inputNum; 
+
+    if(inputNum == 42) 
+    {
+        cout << "The answer to the ultime question of life, the universe and everything!";
+    }
+    else 
+    {
+        cout << "Hello world!";
+    }
+
+    return 0;
+}
